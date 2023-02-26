c90e24f9a88bbde1c8072796ce732c0bd5d80f6b
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2023-02-27 00:51:48 +0600

    Edit feature section

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,15 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { featuresData } from "../data";
+import { FeatureProduct } from "../utils/FeatureProduct";
 export const Features = () => {
+  const { title, list, subtitle } = featuresData;
   return (
-    <div className="container mx-auto">
-      <section>
-        <div className="">
-          <div className="">
-            
+    <section className="my-[70px] xl:my-[150px]">
+      <div className="container mx-auto relative">
+        <div className="text-center">
+          <h2 className="h2 mb-6 xl:mb-0">{title}</h2>
+          <p className="lead max-w-[585px] mx-auto mt-[20px]">{subtitle}</p>
+          <div className="mt-20">
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-[50px]
+            ">
+              {list.map((items, index) => {
+                return <FeatureProduct key={index} items={items} />
+              })}
+            </div>
           </div>
         </div>
-      </section>
-    </div>
-  )
-}
+      </div>
+    </section>
+  );
+};
---
+++ src/utils/FeatureProduct.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BsArrowRight } from 'react-icons/bs';
+
+export const FeatureProduct = (items) => {
+  const { title, image, bgImage, description, delay, linkText} = items.items;
+  
+  return (
+    <div className="w-full max-w-[530px] h-[360px] relative flex flex-col items-center justify-center xl:flex-row xl:justify-start mx-auto">
+      <div className="xl:flex absolute top-0 right-0 -z-20  ">
+        <img src={bgImage} alt="" />
+      </div>
+      <div className="max-w-[120px] xl:mr-7 xl:max-w-[230px] ">
+        <img src={image} alt="" />
+      </div>
+      <div className="max-w-[180px] text-start mb-10 flex flex-col justify-center">
+        <h3 className="h3 mb-4">{title}</h3>
+        <p className="text-start font-light italic gap-x-2 group">
+          {description}
+        </p>
+        <div className="flex  justify-center gap-x-2 group mt-2">
+          <a href="" className="text-primary font-bold">
+            {linkText}
+          </a>
+          <BsArrowRight className="text-xl text-accent-primary group-hover:ml-[5px] transition-all" />
+        </div>
+      </div>
+    </div>
+  );
+}
